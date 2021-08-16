00e4d4cac6fc4dc8b0bfd1debf4379a81a019eb1
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-08-17 03:48:59 +0530

    heading added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import logo from './components/img/logo.svg'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import logo from "./components/img/logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MDBContainer } from "mdbreact";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Navbar className="navb"
-        sticky="top" expand="sm" collapseOnSelect>
-        <Navbar.Brand>
-          <img className="navlogo" src={logo}  />{' '}
-          
-        </Navbar.Brand>
-        {
-/*
+      <div className="App">
+        <Navbar className="navb" sticky="top" expand="sm" collapseOnSelect>
+          <Navbar.Brand>
+            <img className="navlogo" src={logo} />{" "}
+          </Navbar.Brand>
+          {/*
         <Navbar.Toggle className="coloring" />
         <Navbar.Collapse>
           <Nav>
@@ -32,11 +34,22 @@ function App() {
           </Nav>
         </Navbar.Collapse>
 */}
-      </Navbar>
-      <div className="content">
+        </Navbar>
+        
+          
+            <div className="vl" id="vl1"></div>
+            <div className="vl" id="vl2"></div>
+            <div className="vl" id="vl3"></div>
+            <br/><br/><br/><br/><br/>
+            <div style={{ color: "white",width:"50%" }} className="head ">
+              <h1 style={{ width:"100%",fontFamily: "DIN 1451 Std", fontSize:"70px", fontWeight:"bold" }}>The new standard <br/>of <span style={{ color: "red" }}>Design.</span></h1>
+              </div>
+
+
+            
+          
         
       </div>
-    </div>
     </>
   );
 }
