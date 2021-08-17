cd542b0a3cf743224af4b8f8a75fd602766d9a2c
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-08-18 00:26:23 +0530

    added js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import "./App.css";
+import React, { useEffect, useState, useCallback } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import logo from "./components/img/logo.svg";
+import arrow from "./components/img/arrow.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { MDBContainer } from "mdbreact";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 function App() {
   const spinTransition = {
     loop: Infinity,
     ease: "linear",
     duration: 10,
+  };
+  const text = ["Empathise", "Define", "Ideate", "Prototype", "Test"];
+  const [counter,setCounter]=useState(0);
+  const [i,seti]=useState(text[counter]);
+  const [call,setCall]=useState(true);
+
+  
+  useEffect(() => {
+    if(call){
+      change();
+      setCall(false);
+    }
+    const intervalID = setInterval(change, 2000);
+    return () => clearInterval(intervalID);
+  },[change,useState]);
+  
+  // componentDidMount() {
+  //  const interval = setInterval(change(), 2000);
+  // };
+  // componentWillUnmount(){
+  //   clearInterval(interval);
+  // };
+
+  function change(){
+    console.log(counter);
+    setCounter(counter+1);
+    if(counter==text.length-1){
+      setCounter(0);
+    }
+    
+    seti(text[counter]);
+   
   }
   return (
     <>
@@ -42,14 +75,14 @@ function App() {
 */}
         </Navbar>
         <div className="flex-container">
-          <div  className="vl" id="vl1">
+          <div className="vl" id="vl1">
             <div style={{ color: "#F3EFE6", width: "50vw" }} className="head ">
               <h1
                 style={{
                   width: "100%",
                   fontSize: "80px",
                   fontWeight: "bold",
-                  lineHeight:"140%"
+                  lineHeight: "140%",
                 }}
               >
                 The new standard <br />
@@ -58,20 +91,26 @@ function App() {
             </div>
             <div className="head2" style={{ color: "white", width: "50vw" }}>
               <br />
-              <p style={{fontFamily: "IBM Plex Mono"}}>We are IxDA Vellore. We are sabisdubckudbsidu<br/>cbcdskubcse kbdsckubc dsbcdsucsbdudscbdsubcdsujbc</p>
+              <p style={{ fontFamily: "IBM Plex Mono" }}>
+                Changing the future of design.
+              </p>
             </div>
+            <br/><br/><br/><br/>
+            <img className="arrowd" src={arrow} />
+
+            
           </div>
-          <div  className="vl" id="vl2">
-            <motion.div 
+          <div className="vl" id="vl2">
+            <motion.div
               className="circle"
-              animate={{ rotate:360 }}
-              transition ={spinTransition}>
-            </motion.div>
-              <span className="inner-text">Emphasize</span>
+              animate={{ rotate: 360 }}
+              transition={spinTransition}
+            ></motion.div>
+            <span className="inner-text" id="inner-text">{i}</span>
           </div>
-          <div  className="vl" id="vl3"></div>
-          </div>
+          <div className="vl" id="vl3"></div>
         </div>
+      </div>
     </>
   );
 }
