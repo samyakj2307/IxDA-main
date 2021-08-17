b88929db529789b8325540a212ccf01b5a4c183e
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-08-18 01:22:59 +0530

    added color change wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import "./App.css";
 import React, { useEffect, useState, useCallback } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Nav, Navbar } from "react-bootstrap";
-import logo from "./components/img/logo.svg";
+import a from "./components/img/a.svg";
+import b from "./components/img/b.svg";
+import c from "./components/img/c.svg";
+import d from "./components/img/d.svg";
+import e from "./components/img/e.svg";
+
 import arrow from "./components/img/arrow.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -16,9 +21,16 @@ function App() {
     ease: "linear",
     duration: 10,
   };
-  const text = ["Empathise", "Define", "Ideate", "Prototype", "Test"];
+  const text = ["a","b","c","d","e"];
+  const ctext = ["ca","cb","cc","cd","ce"];
+  const itext = [a,b,c,d,e];
+  const circ = ["Empathise", "Define", "Ideate", "Prototype", "Test"];
+
   const [counter,setCounter]=useState(0);
-  const [i,seti]=useState(text[counter]);
+  const [i,seti]=useState(circ[counter]);
+  const [x,setx]=useState(text[counter]);
+  const [y,sety]=useState(ctext[counter]);
+  const [z,setz]=useState(itext[counter]);
   const [call,setCall]=useState(true);
 
   
@@ -27,9 +39,9 @@ function App() {
       change();
       setCall(false);
     }
-    const intervalID = setInterval(change, 2000);
+    const intervalID = setInterval(change, 1950);
     return () => clearInterval(intervalID);
-  },[change,useState]);
+  },[change,useState],);
   
   // componentDidMount() {
   //  const interval = setInterval(change(), 2000);
@@ -39,13 +51,17 @@ function App() {
   // };
 
   function change(){
-    console.log(counter);
+    console.log(text[counter]);
     setCounter(counter+1);
-    if(counter==text.length-1){
+    if(counter==circ.length-1){
       setCounter(0);
     }
     
-    seti(text[counter]);
+    seti(circ[counter]);
+    setx(text[counter]);
+    sety(ctext[counter]);
+    setz(itext[counter]);
+
    
   }
   return (
@@ -53,7 +69,7 @@ function App() {
       <div className="App">
         <Navbar className="navb" sticky="top" expand="sm" collapseOnSelect>
           <Navbar.Brand href="/">
-            <img className="navlogo" src={logo} />{" "}
+            <img className="navlogo" src={itext[counter]} />{" "}
           </Navbar.Brand>
           {/*
         <Navbar.Toggle className="coloring" />
@@ -86,10 +102,10 @@ function App() {
                 }}
               >
                 The new standard <br />
-                of <span style={{ color: "#C02D33" }}>Design.</span>
+                of <span className={text[counter]} >Design.</span>
               </h1>
             </div>
-            <div className="head2" style={{ color: "white", width: "50vw" }}>
+            <div className="head2" style={{ color: "#F3EFE6", width: "50vw" }}>
               <br />
               <p style={{ fontFamily: "IBM Plex Mono" }}>
                 Changing the future of design.
@@ -103,10 +119,11 @@ function App() {
           <div className="vl" id="vl2">
             <motion.div
               className="circle"
+              id={ctext[counter]}
               animate={{ rotate: 360 }}
               transition={spinTransition}
             ></motion.div>
-            <span className="inner-text" id="inner-text">{i}</span>
+            <span className="inner-text" id={text[counter]}>{i}</span>
           </div>
           <div className="vl" id="vl3"></div>
         </div>
