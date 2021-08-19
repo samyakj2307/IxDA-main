3c9a814009d8e8b080a0892cf2bf62ae811fc597
Grihit <grihitbudhiraja12@gmail.com>
2021-08-19 13:24:22 +0530

    Team

---
+++ src/components/Member.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles.css";
+
+function Member({data}) {
+    return (
+        <div>
+            <div className="img-name">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg" alt={data.name}></img>
+                <p>{data.name}</p>
+            </div>
+            <p className="designation">{data.designation}</p>
+            <p className="skill">{data.skill}</p>
+        </div>
+    )
+}
+
+export default Member
