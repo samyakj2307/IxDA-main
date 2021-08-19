6626546a26c4b7580fe8f502e1856e7cdf1f60c3
Grihit <grihitbudhiraja12@gmail.com>
2021-08-19 14:04:18 +0530

    added image border

--- src/components/Member.js
+++ src/components/Member.js
@@ -5,7 +5,7 @@ function Member({data}) {
     return (
         <div>
             <div className="img-name">
-                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg" alt={data.name}></img>
+                <img src={data.image} alt={data.name}></img>
                 <p>{data.name}</p>
             </div>
             <p className="designation">{data.designation}</p>
